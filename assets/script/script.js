0c320124e717583e01d7a1186eeba5d7346d3556
Anna Teilane <kastekastee@gmail.com>
2024-09-14 16:29:54 +0000

    create onClick function for emojis

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,9 +7,9 @@ for ( var i = 0; i < emojis.length; i++){
     let box = document.createElement('div')
     box.className = 'item';
     box.innerHTML = shuf_emojis[i]
-
+/** onClick function for card */
     box.onclick = function(){
-        this.classList.add('boxOpen')
+        this.classList.add('boxOpen');
     }
     document.querySelector('.game').appendChild(box);
 }
