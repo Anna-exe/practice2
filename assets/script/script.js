796c061a0e1a97ae54e583394b7210ad58b98a50
Anna Teilane <kastekastee@gmail.com>
2024-09-14 15:15:58 +0000

    fix file path in js link, add function to display emojis

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,11 @@
 const emojis = [
     '💖', '💖', '🐱‍👤', '🐱‍👤', '⛩','⛩', '🌛','🌛', '🎃', '🎃', '🎁','🎁', '🥋', '🥋', '🐺', '🐺'
 ];
+
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+for ( var i = 0; i < emojis.length; i++){
+    let box = document.createElement('div')
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+    document.querySelector('.game').appendChild(box);
+}
