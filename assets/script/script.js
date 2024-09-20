055410185479580f02d1f69c9fd2c68e22f36340
Anna Teilane <kastekastee@gmail.com>
2024-09-20 15:55:25 +0000

    here

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,10 +12,11 @@ for ( var i = 0; i < emojis.length; i++){
         this.classList.add('boxOpen');
         setTimeout(function(){
             if(document.querySelectorAll('.boxOpen').length > 1){
-                if(document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML){
+                if(document.querySelectorAll('.boxOpen')[0].innerHTML == 
+                document.querySelectorAll('.boxOpen')[1].innerHTML){
                     document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
                     document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
-
+/////////stopped here
                     document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
                     document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
 
